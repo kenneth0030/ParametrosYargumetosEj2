3ae83864902101563492637b2a7a20bf8aa2d465
kenneth pegueros <kenneth_183@hotmail.com>
2023-05-01 16:44:23 -0600

    Initial commit

---
+++ src/main/java/com/mycompany/parametrosyargumetosej2/ParametrosYargumetosEj2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.parametrosyargumetosej2;
+
+/**
+ *
+ * @author kenne
+ */
+public class ParametrosYargumetosEj2 {
+    
+    public void mensaje(String a){
+        System.out.println("el mensaje es:"+a);
+    
+    }
+
+    public static void main(String[] args) {
+        ParametrosYargumetosEj2 eje = new ParametrosYargumetosEj2();
+        eje.mensaje("buenas tardes");
+    }
+}
